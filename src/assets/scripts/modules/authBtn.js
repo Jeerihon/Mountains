e33923a57a9d20a04df6e2228cc5177f27be5955
Jeerihon <korart_95@mail.ru>
2019-01-03 18:33:09 +0800

    upd authBtns

--- src/assets/scripts/modules/authBtn.js
+++ src/assets/scripts/modules/authBtn.js
@@ -1,11 +1,28 @@
 
 (function () {
-    const authBtn = document.getElementById('authBtn'),
-    frontSide = document.querySelector('.side--front'),
-    backSide = document.querySelector('.side--back');
+    const container = document.querySelector('.welcome__container'),
+        authBtn = document.getElementById('authBtn'),
+        frontSide = document.querySelector('.side--front'),
+        backSide = document.querySelector('.side--back'),
+        toMainPageBtn = document.getElementById('toMainPageBtn')
 
-        authBtn.addEventListener('click', function() {
-            frontSide.classList.toggle('rotate');
-            backSide.classList.toggle('rotate');
-        });
+    container.onclick = function (e) {
+        let target = e.target;
+
+        if (target === authBtn || target === toMainPageBtn) {
+            rotate();
+
+            if (target === authBtn) {
+                authBtn.style.display = 'none';
+            } else if (target === toMainPageBtn) {
+                authBtn.style.display = 'block';
+            }
+        }
+    };
+
+    const rotate = function () {
+        frontSide.classList.toggle('rotate');
+        backSide.classList.toggle('rotate');
+    };
+
 })();
