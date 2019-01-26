3eaf55d2273ad49dc7aa3b3e39a240a8fd4ce41e
Jeerihon <korart_95@mail.ru>
2019-01-27 04:43:19 +0800

    auth test

--- src/admin/store/modules/adminPages.js
+++ src/admin/store/modules/adminPages.js
@@ -4,7 +4,7 @@ const adminPages = {
       // {title: "Обо мне", href: "/admin"},
       // {title: "Работы", href: "/admin/works"},
       // {title: "Блог", href: "/admin/blog"}
-      {title: "Обо мне", href: "/dist/admin"},
+      {title: "Обо мне", href: "/dist/admin/"},
       {title: "Работы", href: "/dist/admin/works"},
       {title: "Блог", href: "/dist/admin/blog"}
     ]
