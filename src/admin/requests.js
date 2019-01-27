87cfb20ee23d680cb2df260ddc79b1ed03bb88d1
Jeerihon <korart_95@mail.ru>
2019-01-28 03:19:14 +0800

    token test

--- src/admin/requests.js
+++ src/admin/requests.js
@@ -5,6 +5,6 @@ axios.defaults.baseURL = 'https://webdev-api.loftschool.com';
 const token = localStorage.getItem('token');
 console.log(token);
 
-axios.defaults.header['Authorization'] = '';
+axios.defaults.header['Authorization'] = 'bearer ${token}';
 
 export default axios;
